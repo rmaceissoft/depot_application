62a67daca5e8453dc5e220873fe1b283dbba88af
Reiner Marquez <rmaceissoft@gmail.com>
2012-02-26 14:28:32 -0500

    completed playtime of task B section

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,10 @@ class Product < ActiveRecord::Base
 		:with => %r{\.(gif|jpg|png)$}i,
 		:message => 'must be a URL for GIF, JPG or PNG image.'
 	}
+	validates :title, :length => {
+		:minimum => 10,
+		:too_short => "must have at least %{count} chars"
+	 }
 	
 	default_scope :order => 'title'
 end
