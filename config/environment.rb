1bd954893130b0706c9a156336899b064c213ea3
Reiner Marquez <rmaceissoft@gmail.com>
2012-02-28 13:51:50 -0500

    completed iteration H1

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Demo::Application.initialize!
+
+# common configuration
+
+Demo::Application.configure do 
+	config.action_mailer.delivery_method = :smtp
+	
+	config.action_mailer.smtp_settings = {
+		:address => "localhost",
+		:port => 25,
+	}	
+end
