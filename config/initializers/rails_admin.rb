63b632d9d649f39061a7baa1c73502ce8e639782
Reiner Marquez <rmaceissoft@gmail.com>
2012-03-01 14:04:12 -0500

    customized PaymentType model at rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -5,4 +5,23 @@ RailsAdmin.config do |c|
   c.current_user_method { User.first }
   c.authorize_with {}
   c.audit_with :history, User
+  
+  c.model PaymentType do
+    list do
+      field :name
+      field :created_at
+      field :updated_at
+    end
+    
+    create do
+    	fields :name
+    end
+    
+    edit do
+    	fields :name
+    end
+
+  end
+  
+
 end
